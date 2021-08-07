250c5792bd68670fa0099447642831f432704380
Ben McClure <ben.mcclure@gmail.com>
2021-08-07 17:50:57 -0400

    Remove unused batch files

--- Scripts/Build.bat
+++
@@ -1,5 +0,0 @@
-if "%AHK_2%"=="" (
-    start "" /D "%CD%" "Build.ahk"
-) else (
-    "%AHK_2%" "%CD%\Build.ahk"
-)
--- Scripts/UpdateIncludes.bat
+++
@@ -1,5 +0,0 @@
-if "%AHK_2%"=="" (
-    start "" /D "%CD%" "UpdateIncludes.ahk"
-) else (
-    "%AHK_2%" "%CD%\UpdateIncludes.ahk"
-)
