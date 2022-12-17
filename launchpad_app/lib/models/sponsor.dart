b419d99c2d33bb44b1d54d0be9ad9bf546f8e4f9
Ben McClure <ben.mcclure@gmail.com>
2022-12-17 16:34:25 -0500

    Create a Sponsors model based on fluent_ui's example

---
+++ launchpad_app/lib/models/sponsor.dart
@@ -0,0 +1,49 @@
+/// Current sponsors of Launchpad
+const sponsors = [
+  Sponsor(
+    username: 'bmcclure',
+    imageUrl: 'https://avatars.githubusercontent.com/u/277977?v=4',
+    name: 'Sander in \'t Hout',
+  )
+];
+
+class Sponsor {
+  final String? username;
+  final String name;
+  final String imageUrl;
+
+  const Sponsor({
+    required this.username,
+    required this.name,
+    required this.imageUrl,
+  });
+
+  Sponsor copyWith({
+    String? username,
+    String? name,
+    String? imageUrl,
+  }) {
+    return Sponsor(
+      username: username ?? this.username,
+      name: name ?? this.name,
+      imageUrl: imageUrl ?? this.imageUrl,
+    );
+  }
+
+  @override
+  String toString() =>
+      'Sponsor(username: $username, name: $name, imageUrl: $imageUrl)';
+
+  @override
+  bool operator ==(Object other) {
+    if (identical(this, other)) return true;
+
+    return other is Sponsor &&
+        other.username == username &&
+        other.name == name &&
+        other.imageUrl == imageUrl;
+  }
+
+  @override
+  int get hashCode => username.hashCode ^ name.hashCode ^ imageUrl.hashCode;
+}
